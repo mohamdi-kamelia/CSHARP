317d8de3a7ff2020e205aec9fa52795654d145ce
jean-renoul <jean.renoul@laplateforme.io>
2024-10-02 13:22:09 +0200

    map loads succesfully, assets copied in bin

--- Empire Deluxe/obj/Debug/net8.0/Empire Deluxe.AssemblyInfo.cs
+++ Empire Deluxe/obj/Debug/net8.0/Empire Deluxe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Empire Deluxe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882d24b371bd1f4fc5832d065ebe2b15e1b2beb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e133483f8381a051f224d94a822efecf36bf7f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Empire Deluxe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Empire Deluxe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
